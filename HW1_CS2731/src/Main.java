5c7e383793d06a203d54edb4398f08d134fca8a6
Zhongxuan Song <zhs61@pitt.edu>
2019-09-13 00:43:27 -0400

    bi and tri completed

--- HW1_CS2731/src/Main.java
+++ HW1_CS2731/src/Main.java
@@ -6,9 +6,15 @@ import java.util.Scanner;
 
 public class Main {
 
-	private static Map<Character, Integer> charMap = new HashMap<>();
+	private static Map<String, Integer> charMap = new HashMap<>();
+	private static Map<String, Integer> biCharMap = new HashMap<>();
+	private static Map<String, Integer> triCharMap = new HashMap<>();
 	private static Map<String, Double> unigramModel = new HashMap<>();
+	private static Map<String, Double> bigramModel = new HashMap<>();
+	private static Map<String, Double> trigramModel = new HashMap<>();
+	
 	private static int charCount = 0;
+	
 	public static void main(String[] args) {
 
 		// read the file line by line
@@ -23,10 +29,77 @@ public class Main {
 			while (sc.hasNextLine()) {
 				String token = sc.nextLine();
 				fillCharMap(token);
+				fillBiCharMap(token);
+				fillTriCharMap(token);
 			}
-			getUnigramMode();
+			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
+		}
+		getUnigramModel();
+		getBigramModel();
+		getTrigramModel();
+		System.out.println();
+	}
+
+	private static void getTrigramModel() {
+		for (Map.Entry<String, Integer> entry : triCharMap.entrySet()) {
+			String key = entry.getKey();
+			String base = key.charAt(0) +"" + key.charAt(1);
+			int baseCount = biCharMap.get(base);
+			double pro = entry.getValue() * 1.0 / baseCount;
+			trigramModel.put(key, pro);
+		} 
+		
+	}
+
+	/**
+	 * fill the trigramcharmap 
+	 * @param token
+	 */
+	private static void fillTriCharMap(String token) {
+		for (int i = 0; i < token.length()-2; i++) {
+			String first = token.charAt(i)+"";
+			String second = token.charAt(i+1)+"";
+			String third = token.charAt(i+2)+"";
+			String triChar = first + second + third;
+			if (triCharMap.containsKey(triChar)) {
+				triCharMap.put(triChar, triCharMap.get(triChar)+1);
+			} else {
+				triCharMap.put(triChar, 1);
+			}
+		}
+		
+	}
+
+	/**
+	 * use the data from in BicharMap to generated a bigram model
+	 */
+	private static void getBigramModel() {
+		for (Map.Entry<String, Integer> entry : biCharMap.entrySet()) {
+			String key = entry.getKey();
+			String base = String.valueOf(key.charAt(0));
+			int baseCount = charMap.get(base);
+			double pro = entry.getValue() * 1.0 / baseCount;
+			bigramModel.put(key, pro);
+		} 
+		
+	}
+
+	/**
+	 * count the number of two chars
+	 * @param token
+	 */
+	private static void fillBiCharMap(String token) {
+		for (int i = 0; i < token.length()-1; i++) {
+			String first = token.charAt(i)+"";
+			String second = token.charAt(i+1) + "";
+			String biChar = first + second;
+			if (biCharMap.containsKey(biChar)) {
+				biCharMap.put(biChar, biCharMap.get(biChar)+1);
+			} else {
+				biCharMap.put(biChar, 1);
+			}
 		}
 		
 	}
@@ -36,19 +109,19 @@ public class Main {
 	 * use the count of each character divided by the total count
 	 * store the result in a hashmap with the char as the key
 	 */
-	private static void getUnigramMode() {
-		for (Map.Entry<Character, Integer> entry : charMap.entrySet()) {
-			String key = String.valueOf(entry.getKey());
+	private static void getUnigramModel() {
+		for (Map.Entry<String, Integer> entry : charMap.entrySet()) {
+			String key = entry.getKey();
 			double pro = entry.getValue() * 1.0 / charCount;
 			unigramModel.put(key, pro);
-		}
+		} 
 		
 	}
 
 	private static void fillCharMap(String token) {
 		
 		for (int i = 0; i < token.length(); i++) {
-			char c = token.charAt(i);
+			String c = token.charAt(i) + "";
 			charCount++;
 			if (charMap.containsKey(c)) {
 				charMap.put(c, charMap.get(c)+1);
