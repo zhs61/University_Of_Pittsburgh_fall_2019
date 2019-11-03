37b9ebf8db6b65d18fdefde1aa062c283de5f6fa
songz2 <songz2@miamioh.edu>
2019-11-03 15:15:49 -0500

    gg

--- PyCharmProjects/HW3_CS2731/HW3.py
+++ PyCharmProjects/HW3_CS2731/HW3.py
@@ -1,23 +1,62 @@
-import re
+import gensim
 import nltk
-from sklearn.ensemble import VotingClassifier
-
-nltk.download('punkt')
-from nltk.tokenize import word_tokenize
-nltk.download('stopwords')
-from nltk.corpus import stopwords
+import numpy as np
 from nltk.stem.porter import PorterStemmer
 stemmer = PorterStemmer()
-from autocorrect import spell
-import numpy as np
 import pandas as pd
-import xlrd
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
+from pyspark.mllib.linalg import SparseVector, DenseVector
+from warnings import simplefilter
+simplefilter(action='ignore', category=FutureWarning)
+from gensim.models import Word2Vec
+
+#===================
+
+class MyTokenizer:
+    def __init__(self):
+        pass
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        transformed_X = []
+        for document in X:
+            tokenized_doc = []
+            for sent in nltk.sent_tokenize(document):
+                tokenized_doc += nltk.word_tokenize(sent)
+            transformed_X.append(np.array(tokenized_doc))
+        return np.array(transformed_X)
+
+    def fit_transform(self, X, y=None):
+        return self.transform(X)
 
 
+class MeanEmbeddingVectorizer(object):
+    def __init__(self, word2vec):
+        self.word2vec = word2vec
+        # if a text is empty we should return a vector of zeros
+        # with the same dimensionality as all the other vectors
+        self.dim = len(word2vec.wv.syn0[0])
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X = MyTokenizer().fit_transform(X)
+
+        return np.array([
+            np.mean([self.word2vec.wv[w] for w in words if w in self.word2vec.wv]
+                    or [np.zeros(self.dim)], axis=0)
+            for words in X
+        ])
+
+    def fit_transform(self, X, y=None):
+        return self.transform(X)
+
+#=========================
 punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
 list_of_words = {}
 bag_of_word = {}
@@ -50,28 +89,66 @@ def remove_punctuation(features):
         result.append(no_punct)
     return result
 
+def get_bow(no_punc_features):
+    cvectorizer = CountVectorizer()
+    bow_noarray = cvectorizer.fit_transform(no_punc_features)
+    bow = bow_noarray.toarray()
+    return bow
+
+
+def get_tf_idf(no_punc_features):
+    tfidf_vectorizer = TfidfVectorizer(use_idf=True)
+    tfidf_vectorizer_vectors = tfidf_vectorizer.fit_transform(no_punc_features)
+    tfidf = tfidf_vectorizer_vectors.toarray()
+    return tfidf
+
+def cross_validation(text_representation, toxi, train_size, test_size):
+    #X_train, X_test, y_train, y_test = train_test_split(text_representation, toxi, train_size=train_size, test_size=test_size, random_state=0)
+    score = []
+    text_representation = np.array(text_representation)
+    toxi = np.array(toxi)
+    kf = KFold(n_splits=10, shuffle=True)
+    for train_index, test_index in kf.split(text_representation):
+        X_train, X_test, y_train, y_test = text_representation[train_index], text_representation[test_index], toxi[train_index], toxi[test_index]
+        classifier = get_classifier(X_train, y_train)
+        score.append(classifier.score(X_test, y_test))
+    return score
+
+
+def get_classifier(X_train, y_train):
+    classifier = LogisticRegression(solver='lbfgs', multi_class='auto', max_iter=1500)
+    classifier.fit(X_train, y_train)
+    return classifier
+
 
 def main():
     features, toxi = read_file()
     no_punc_features = remove_punctuation(features)
-    vectorizer = CountVectorizer()
-    bow = vectorizer.fit_transform(no_punc_features).toarray()
-    X_train, X_test, y_train, y_test = train_test_split(bow, toxi, test_size=0.3, random_state=0)
-    bow_classifier = LogisticRegression()
-    bow_classifier.fit(X_train, y_train)
-    y_pred = bow_classifier.predict(X_test)
-    print(bow_classifier.score(X_test, y_test))
+    bow = get_bow(no_punc_features)
+    score = cross_validation(bow, toxi, 0.8, 0.2)
+    print(np.mean(score))
 
+    tfidf = get_tf_idf(no_punc_features)
+    score = cross_validation(tfidf, toxi, 0.8, 0.2)
+    print(np.mean(score))
 
-    # majority_classifier = VotingClassifier(estimators=[X_train, y_train])
-    # majority_classifier.fit(X_train, y_train)
-    # print(majority_classifier.score(X_test, y_test))
-
-    #get_list_of_word(no_punc_features)
-    #bag_of_word_preprocess(no_punc_features)
-    # for x in no_punc_features:
-    #     print(x)
+    # all_words = [nltk.word_tokenize(sent) for sent in no_punc_features]
+    # word2vec = Word2Vec(no_punc_features, min_count=2, workers=4)
+    # max_dataset_size = len(word2vec.wv.syn0)
+    # #score = cross_validation(word2vec.wv.syn0, toxi[:max_dataset_size], 0.8, 0.2)
+    # X_train, X_test, y_train, y_test = train_test_split(word2vec.wv, toxi[:max_dataset_size], train_size=0.8, test_size=0.2, random_state=0)
+    # classifier = get_classifier(X_train, y_train)
+    # score = classifier.score(X_test, y_test)
+    # print(np.mean(score))
+    model = gensim.models.Word2Vec(no_punc_features, workers=4)
+    mean_embedding_vectorizer = MeanEmbeddingVectorizer(model)
+    mean_embedded = mean_embedding_vectorizer.fit_transform(no_punc_features)
+    #X_train, X_test, y_train, y_test = train_test_split(mean_embedded, toxi, train_size=0.8,test_size=0.2, random_state=0)
+    score = cross_validation(mean_embedded, toxi, 0.8, 0.2)
+    print(np.mean(score))
 
 
 if __name__ == '__main__':
     main()
+
+
