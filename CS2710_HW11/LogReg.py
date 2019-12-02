1cc19484bd78403aa8c768c053e6341d5dc1947d
Zhongxuan Song <zhs61@pitt.edu>
2019-12-02 00:06:12 -0500

    cs2710 hw11 need some work

--- CS2710_HW11/LogReg.py
+++ CS2710_HW11/LogReg.py
@@ -7,11 +7,10 @@ from LogReg_data_load import x_train, y_train, x_test, y_test
 
 def LogReg_probability(w, x):
     sum = 0
-    w0 = w[0]
     for i in range(len(x)):
-        sum = sum + w[i+1] * x[i]
-    result = 1 / (1 + math.exp(-(w0 + sum)))
-    return float(result)
+        sum = sum + w[i] * x[i]
+    result = 1 / (1.0 + math.exp(-sum))
+    return result
 
 def LogReg_online_GD(x_train, y_train, num_iterations):
     w = [0 for i in range(len(x_train[0])+1)]
@@ -19,15 +18,16 @@ def LogReg_online_GD(x_train, y_train, num_iterations):
         learning_rate = 1 / (2 * math.sqrt(i+1))
         index = random.randrange(0, len(x_train))
         row = x_train[index]
-        label = y_train[index][0]
-        prob = (label-LogReg_probability(w, row))
+        row = np.insert(row, 0, 1)
+        row = row.tolist()
+        label = y_train[index]
+        prob = (label[0]-LogReg_probability(w, row))
         for j in range(len(row)):
-            w[j+1] = w[j+1] + learning_rate * prob * row[j]
+            w[j] = w[j] + learning_rate * prob * row[j]
     return w
 
 def LogReg_classify(w, x):
     prob = LogReg_probability(w, x)
-    #print(prob)
     if prob >= 0.5:
         return 1
     else:
@@ -39,6 +39,6 @@ def mis_error(predictedlabels, truelabels):
     for i in range(len(predictedlabels)):
         if predictedlabels[i] != truelabels[i][0]:
             misclassified += 1
-    return misclassified * 1.0 / len(predictedlabels)
+    return misclassified / len(predictedlabels)
 
 
--- CS2710_HW11/LogReg_Run.py
+++ CS2710_HW11/LogReg_Run.py
@@ -3,19 +3,41 @@ from LogReg_data_load import *
 
 if __name__=='__main__':
     w_train = LogReg_online_GD(x_train, y_train, 1500)
-    w_test = LogReg_online_GD(x_test, y_test, 1500)
-    train_miscal = 0
-    for j in range(20):
-        output = []
-        for i in range(len(x_train)):
-            output.append(LogReg_classify(w_train, x_train[i]))
-        train_miscal += mis_error(output, y_train)
-    print(train_miscal/20)
 
+    # Part b: print the weight
+    print(w_train)
+
+    # part c test
+    for z in range(30):
+        print(LogReg_classify(w_train, x_test[z]))
+    for z in range(30):
+        print(y_test[z][0])
+
+    # part d:
+    train_output = []
+    for i in range(len(x_train)):
+        train_output.append(LogReg_classify(w_train, x_train[i]))
+    print(mis_error(train_output, y_train))
+
+    test_output = []
+    for i in range(len(x_test)):
+        test_output.append(LogReg_classify(w_train, x_test[i]))
+    print(mis_error(test_output, y_test))
+
+    # part e
+    train_miscal = 0
     test_miscal = 0
     for j in range(20):
-        output = []
+        w_train = LogReg_online_GD(x_train, y_train, 1500)
+        tr_output = []
+        for i in range(len(x_train)):
+            tr_output.append(LogReg_classify(w_train, x_train[i]))
+        train_miscal += mis_error(tr_output, y_train)
+
+        ts_output = []
         for i in range(len(x_test)):
-            output.append(LogReg_classify(w_test, x_test[i]))
-        test_miscal += mis_error(output, y_test)
+            ts_output.append(LogReg_classify(w_train, x_test[i]))
+        test_miscal += mis_error(ts_output, y_test)
+    print(train_miscal/20.0)
     print(test_miscal / 20.0)
+
